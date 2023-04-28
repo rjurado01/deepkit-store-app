fecd249c954d8af480596ff997d81fd1ecdb22ec
rjurado01 <rjurado01@gmail.com>
2023-04-28 17:25:12 +0200

    Authentication

--- src/apps/web-api/controllers/products-create.controller.ts
+++ src/apps/web-api/controllers/products-create.controller.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express'
+import {Response} from 'express'
 
 import {
   CreateProductDto,
@@ -6,12 +6,15 @@ import {
 } from '../../../core/product/app/create-product.service'
 
 import {injectable} from '../../../shared/decorators/injectable'
+// import {authenticated} from '../guards/authenticated.guard'
+import {CustomRequest} from '../interfaces'
 
 @injectable()
 export class ProductsCreateController {
   constructor(private service: CreateProductService) {}
 
-  async run(req: Request, res: Response) {
+  // @authenticated()
+  async run(req: CustomRequest, res: Response) {
     const dto = new CreateProductDto(req.body)
 
     await this.service.run(dto)
